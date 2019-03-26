4c3a25d9fc82c8c47ffcd17b69f3e43e78440fe8
Jose Zalacain <Jose Zalacain@AVALON>
2019-03-26 16:49:45 -0400

    Rename project

--- src/main/java/org/prolobjectlink/web/platform/AbstractJettyServer.java
+++ src/main/java/org/prolobjectlink/web/platform/AbstractJettyServer.java
@@ -54,11 +54,9 @@ public abstract class AbstractJettyServer extends AbstractWebServer implements J
 	}
 
 	public final void start() {
-		System.out.println("Server is starting");
 		try {
 			jettyServer.start();
 			jettyServer.join();
-			System.out.println("Server is started");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
